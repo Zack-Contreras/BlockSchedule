c47dee35a1d6ae684d254f5be7a224abec8f8fbf
Zackary Contreras <zackarycontreras@Zackarys-MacBook-Pro.local>
2018-12-06 22:18:39 -0600

    My first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+function ageChecker(){
+  age = prompt("How old are you?")
+  if(age < 21) {
+    alert("go home child");
+  } else {
+    alert("LET'S GOOO");
+  }
+}
+ageChecker();
+*/
+function styleChange(){
+  document.getElementById("lineThru").style.display = 'none';
+}
+function styleChange1(){
+  document.getElementById("showNone").style.display = 'none';
+}
+function styleChange2(){
+  document.getElementById("showNone2").style.display = 'none';
+}
+function styleChange3(){
+  document.getElementById("showNone3").style.display = 'none';
+}
+function styleChange4(){
+  document.getElementById("showNone4").style.display = 'none';
+}
+function deleteBox(){
+  document.getElementById("dltbox").style.display = 'none';
+}
+function styleChange5(){
+  document.getElementById("ex1", "ex2").style.display = 'none';
+}
+function styleChange6(){
+  var x = document.getElementsByClassName("change1");
+  x[0].style.display = "none";
+}
